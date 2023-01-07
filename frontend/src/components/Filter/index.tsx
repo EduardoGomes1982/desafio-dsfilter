227632acdcb17edafaa950c721f5ff0ba7f8154f
EduardoGomes1982 <edu1982talk@gmail.com>
2023-01-07 14:56:12 -0300

    Filter Form

--- frontend/src/components/Filter/index.tsx
+++ frontend/src/components/Filter/index.tsx
@@ -2,8 +2,12 @@ import "./styles.css";
 
 export default function Filter() {
     return(
-        <section id="filter-section">
-            
+        <section id="filter-section" className="ds-container ds-filter-card">
+            <form id="filter-frm">
+                <input id="min-price" name="minPrice" type="text" placeholder="Preço mínimo" />
+                <input id="max-price" name="maxPrice" type="text" placeholder="Preço máximo" />
+                <input type="submit" value="Filtrar" />
+            </form>
         </section>
     )
 }
