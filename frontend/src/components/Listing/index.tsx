4994ec3d55bfc53ae49cf5debb5db9ff36cdce7d
EduardoGomes1982 <edu1982talk@gmail.com>
2023-01-07 16:48:26 -0300

    Listing Card

--- frontend/src/components/Listing/index.tsx
+++ frontend/src/components/Listing/index.tsx
@@ -2,8 +2,11 @@ import "./styles.css";
 
 export default function Listing() {
     return(
-        <section id="listing-section">
-            
+        <section id="listing-section" className="ds-container ds-listing-card">
+            <div className="dsc-list-item">
+                <p className="ds-item-title">PC Gamer Pro</p>
+                <p className="ds-item-price">R$ 1200.00</p>
+            </div>
         </section>
     )
 }
