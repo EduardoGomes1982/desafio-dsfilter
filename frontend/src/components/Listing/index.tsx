da6271a1e6a94be2aff163d5f00cc42cbf6e4ff9
EduardoGomes1982 <edu1982talk@gmail.com>
2023-01-07 17:37:36 -0300

    Listing data model

--- frontend/src/components/Listing/index.tsx
+++ frontend/src/components/Listing/index.tsx
@@ -1,12 +1,20 @@
+import { ProductDTO } from "../../models/product";
+import { findByPrice } from "../../services/product-service";
 import "./styles.css";
 
 export default function Listing() {
-    return(
+    const listProd: ProductDTO[] = findByPrice(0, Number.MAX_VALUE);
+
+    return (
         <section id="listing-section" className="ds-container ds-listing-card">
-            <div className="dsc-list-item">
-                <p className="ds-item-title">PC Gamer Pro</p>
-                <p className="ds-item-price">R$ 1200.00</p>
-            </div>
+            {
+                listProd.map(p => 
+                    <div key={p.id} className="dsc-list-item">
+                        <p className="ds-item-title">{p.name}</p>
+                        <p className="ds-item-price">R$ {p.price.toFixed(2)}</p>
+                    </div>
+                )
+            }
         </section>
     )
 }
